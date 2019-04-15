d77a1758cc1773a3b34ab0cf24d2e8f6fb2cce6e
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-15 20:15:15 -0300

    Init project

---
+++ src/main/java/com/andremorita/java/SpringbootRestIonicApplication.java
@@ -0,0 +1,13 @@
+package com.andremorita.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andremorita/java/SpringbootRestIonicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andremorita.java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestIonicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
