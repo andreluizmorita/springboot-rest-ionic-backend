85b8fdcb0e3bb6b3fb959b2a4ea7cf1473ead785
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-16 20:25:31 -0300

    Custom exception handler

--- src/main/java/com/andremorita/java/services/CategoriaService.java
+++ src/main/java/com/andremorita/java/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.andremorita.java.domain.Categoria;
 import com.andremorita.java.repositories.CategoriaRepository;
+import com.andremorita.java.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repository.findById(id); 
-		return obj.orElse(null);
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
