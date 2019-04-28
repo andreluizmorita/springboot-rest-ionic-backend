0e5e499595f4e288235c411cb209ff879a4fd1fd
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-28 14:47:44 -0300

    Delete cliente in cascade

--- src/main/java/com/andremorita/java/services/ClienteService.java
+++ src/main/java/com/andremorita/java/services/ClienteService.java
@@ -63,7 +63,7 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir poruqe há entidades relacionadas!");
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
 		}
 
 	}
