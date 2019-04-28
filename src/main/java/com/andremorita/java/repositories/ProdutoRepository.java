37f5adc00d44c652097b3008d7e64535debbf626
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-28 17:42:44 -0300

    Produtos created search pagination

--- src/main/java/com/andremorita/java/repositories/ProdutoRepository.java
+++ src/main/java/com/andremorita/java/repositories/ProdutoRepository.java
@@ -1,11 +1,22 @@
 package com.andremorita.java.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andremorita.java.domain.Categoria;
 import com.andremorita.java.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+// --- QUERY EXAMPLE
+//	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+//	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
